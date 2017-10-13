f533f383ad76aefc0c38f36a69ce479db98f0021
BugleJones <spencerjhamilton@gmail.com>
2017-10-13 19:39:03 +0000

    Added review partial, test and adds to db, now to tinker and add validations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,19 @@
 class ReviewsController < ApplicationController
 
     def create
-        raise "Yay, I'm here!"
+        @product = Product.find(params[:product_id])
+        @review = @product.reviews.create(review_params)
+
+        if @review.save
+            redirect_to @product, notice: 'Review has been submitted!'
+        else
+            redirect_to @product, notice: "Errors"
+        end
+    end
+
+    private
+    def review_params
+        params.require(:review).permit(:description, :rating).merge(user: current_user)
     end
 
 end

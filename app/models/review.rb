b09b4921fbcb8e4ced6582aa6938815be3e7175e
BugleJones <spencerjhamilton@gmail.com>
2017-10-14 01:55:45 +0000

    Added ratings on the product show page

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,10 @@ class Review < ActiveRecord::Base
     belongs_to :product
     belongs_to :user
 
-    validates :product_id, :user_id, :description, presence: true
+    validates :product_id, :user_id, :description, :rating, presence: true
+
+    def has_zero_ratings?
+        rating == 0
+    end
 
 end

a2c8d858a6cfe6447ca269e16ca7268ca85ff8b2
BugleJones <spencerjhamilton@gmail.com>
2017-10-16 20:40:19 +0000

    Updated spec to include valid product test with all fields

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,29 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  
-  before(:each) do
-    @product = Product.new
-  end
 
-  it 'should be invalid without a name' do
-    expect(@product).to_not be_valid
-    expect(@product.errors.messages[:name]).to include("can't be blank")
-  end
+  describe '.product' do
+    before(:each) do
+      @product = Product.new
+      @category = Category.new name: 'Apparel'
+    end
 
-  it 'should be invalid without a price' do
-    expect(@product).to_not be_valid
-    expect(@product.errors.messages[:price]).to include("can't be blank")
-  end
+    it 'should save with all fields filled in' do
+      @full_product = Product.new(name: 'a', price: '3449', quantity: '3', category: @category)
+      @full_product.save!
+      expect(@full_product).to be_valid
+    end
 
-  it 'should be invalid without a quantity' do
-    expect(@product).to_not be_valid
-    expect(@product.errors.messages[:quantity]).to include("can't be blank")
-  end
+    it 'should be invalid without a name' do
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:name]).to include('can\'t be blank')
+    end
 
-  it 'should be invalid without a category' do
-    expect(@product).to_not be_valid
-    expect(@product.errors.messages[:category]).to include("can't be blank")
+    it 'should be invalid without a price' do
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:price]).to include('can\'t be blank')
+    end
+
+    it 'should be invalid without a quantity' do
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:quantity]).to include('can\'t be blank')
+    end
+
+    it 'should be invalid without a category' do
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:category]).to include("can\'t be blank")
+    end
   end
   
 end

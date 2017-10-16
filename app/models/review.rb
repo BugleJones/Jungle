a743c31e76d092637e3157ada9e05f6dc5333721
BugleJones <spencerjhamilton@gmail.com>
2017-10-16 19:28:04 +0000

    Added rspec functionality - onto testing Jungle's classes and methods

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,10 +3,9 @@ class Review < ActiveRecord::Base
     belongs_to :product
     belongs_to :user
 
-    validates :product_id, :user_id, :description, :rating, presence: true
-
-    def has_zero_ratings?
-        rating == 0
-    end
+    validates :product_id, presence: true
+    validates :user_id, presence: true
+    validates :description, presence: true
+    validates :rating, presence: true
 
 end
